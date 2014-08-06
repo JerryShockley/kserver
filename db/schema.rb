277b2fc11265df2c5443a5ac3ec030e513004504
Jerry Shockley <jerry@shockleynet.com>
2014-08-06 10:13:49 -0400

    converted default User name column to first_name and last_name columns to coincide with profile names

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140805161003) do
+ActiveRecord::Schema.define(version: 20140806005953) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,7 +29,8 @@ ActiveRecord::Schema.define(version: 20140805161003) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "name"
+    t.string   "first_name"
+    t.string   "last_name"
     t.integer  "role"
   end
 
