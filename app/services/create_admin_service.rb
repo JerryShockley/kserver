5315b42238d9fbeeb87982a89b58d4b9339b31ca
Jerry Shockley <jerry@shockleynet.com>
2014-08-06 10:09:35 -0400

    Initial

--- app/services/create_admin_service.rb
+++ app/services/create_admin_service.rb
@@ -1,6 +1,8 @@
 class CreateAdminService
   def call
+    puts "made it to call:  #{Rails.application.secrets.admin_email}"
     user = User.find_or_create_by!(email: Rails.application.secrets.admin_email) do |user|
+      puts"enter loop"
         user.password = Rails.application.secrets.admin_password
         user.password_confirmation = Rails.application.secrets.admin_password
         user.admin!
