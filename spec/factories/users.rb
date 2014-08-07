269a244325dcbd679bf59769896da9a2ed50292e
Jerry Shockley <jerry@shockleynet.com>
2014-08-07 14:21:13 -0400

    Created user factories

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,12 +1,36 @@
 FactoryGirl.define do
   factory :user do
-    name "Test User"
-    email "test@example.com"
-    password "please123"
+    first_name { role.to_s}
+    last_name "User"
+    sequence(:email) {|n| "#{role.to_s}#{n}@shockleynet.com"}
+    # email {"#{role.to_s}@shockleynet.com"}
+    password "foobar"
+    password_confirmation "foobar"
 
-    trait :admin do
-      role 'admin'
+    trait :sysadmin do
+      role :sysadmin
+    end
+    
+    trait :administrator do
+      role :administrator
+    end
+    
+    trait :teacher do
+      role :teacher
+    end
+    
+    trait :parent do
+      role :parent1
+    end
+    
+    trait :student do
+      role :student
     end
 
+    factory :sysadmin, traits: [:sysadmin]
+    factory :administrator, traits: [:administrator]
+    factory :teacher, traits: [:teacher]
+    factory :parent, traits: [:parent]
+    factory :student, traits: [:student]
   end
 end
