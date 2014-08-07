67ba6ddeaa0e31fed655037abc287937d63c3899
Jerry Shockley <jerry@shockleynet.com>
2014-08-07 14:22:23 -0400

    Added config to run Factorygirl Lint before each suite

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -17,9 +17,13 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
 
+# Cucumber
+# World(FactoryGirl::Syntax::Methods)
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.include FactoryGirl::Syntax::Methods
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
@@ -40,4 +44,9 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
+
+  config.before(:suite) do
+      FactoryGirl.lint
+    end
 end
+# rspec
