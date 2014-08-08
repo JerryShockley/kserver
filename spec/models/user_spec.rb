275c8df3439c427144299d000a517eeaefd18cc2
Jerry Shockley <jerry@shockleynet.com>
2014-08-08 10:04:01 -0400

    Switched test to use build_stubbed where applicable

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,15 +1,11 @@
 describe User do
 
-  before(:each) { @user = create(:sysadmin)}
+  let(:sadmin) {build_stubbed :sysadmin}
 
-  # subject { @user }
-
-    describe "#email" do
-    end
 
     describe "validations" do
       it "is valid with a valid object" do
-        expect(build(:sysadmin).valid?).to be_truthy
+        expect(sadmin.valid?).to be_truthy
       end
       it "is invalid without email" do
         expect(valid_field?(:sysadmin, email: nil)).to be_falsey
@@ -17,7 +13,7 @@ describe User do
 
       it "is invalid with a duplicate email address" do
         usr = build(:sysadmin)
-        usr.save
+        usr.save!
         expect(valid_field?(:sysadmin, email: usr.email)).to be_falsey
         
       end
@@ -30,26 +26,26 @@ describe User do
   
     describe "#admin? method" do
       it "returns true for administrator" do
-        usr = create(:administrator)
+        usr = build_stubbed(:administrator)
         expect(usr.admin?).to be_truthy
       end
   
       it "returns true for sysadmin" do
-        expect(@user.admin?).to be_truthy
+        expect(sadmin.admin?).to be_truthy
       end
   
       it "returns false for teacher" do
-        usr = create(:teacher)
+        usr = build_stubbed(:teacher)
         expect(usr.admin?).to be_falsey
       end
   
       it "returns false for Parent" do
-        usr = create(:parent)
+        usr = build_stubbed(:parent)
         expect(usr.admin?).to be_falsey
       end
   
       it "returns false for Student" do
-        usr = create(:student)
+        usr = build_stubbed(:student)
         expect(usr.admin?).to be_falsey
       end
     end
@@ -57,26 +53,26 @@ describe User do
   
     describe "#faculty? method" do
       it "returns true for administrator" do
-        usr = create(:administrator)
+        usr = build_stubbed(:administrator)
         expect(usr.faculty?).to be_truthy
       end
   
       it "returns true for sysadmin" do
-        expect(@user.faculty?).to be_truthy
+        expect(sadmin.faculty?).to be_truthy
       end
   
       it "returns true for teacher" do
-        usr = create(:teacher)
+        usr = build_stubbed(:teacher)
         expect(usr.faculty?).to be_truthy
       end
   
       it "returns false for Parent" do
-        usr = create(:parent)
+        usr = build_stubbed(:parent)
         expect(usr.faculty?).to be_falsey
       end
   
       it "returns false for Student" do
-        usr = create(:student)
+        usr = build_stubbed(:student)
         expect(usr.faculty?).to be_falsey
       end
     end
@@ -91,7 +87,7 @@ end
 # and fals otherwise.
 
 def valid_field?(class_sym, field_hash)
-  usr = build(class_sym, field_hash)
+  usr = build_stubbed(class_sym, field_hash)
   usr.valid?
   !usr.errors.has_key?(field_hash.keys.first)
 end
