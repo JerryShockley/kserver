2bb69f1aa1234ed1bbefc408d7534cd684631129
Jerry Shockley <jerry@shockleynet.com>
2014-08-10 09:20:59 -0400

    Renamed instance variable and used email values to determine instance equality for #show

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -2,8 +2,9 @@ class UserPolicy
   attr_reader :current_user, :model
 
   def initialize(current_user, model)
+    raise Pundit::NotAuthorizedError, "must be logged in" unless current_user 
     @current_user = current_user
-    @user = model
+    @model = model
   end
 
   def index?
@@ -11,7 +12,7 @@ class UserPolicy
   end
 
   def show?
-    @current_user.admin? or @current_user == @user
+    @current_user.admin? or @current_user.email == @model.email
   end
 
   def update?
@@ -19,7 +20,7 @@ class UserPolicy
   end
 
   def destroy?
-    return false if @current_user == @user
+    return false if @current_user == @model
     @current_user.admin?
   end
 
