2ec228197ec5c5a18d92355b0a0041263fa7acf3
Jerry Shockley <jerry@shockleynet.com>
2014-09-08 18:46:46 -0400

    Added strong params support for new first_name and last_name.

--- config/initializers/devise_permitted_parameters.rb
+++ config/initializers/devise_permitted_parameters.rb
@@ -8,9 +8,10 @@ module DevisePermittedParameters
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :first_name << :last_name
-    devise_parameter_sanitizer.for(:account_update) << :first_name << :last_name
-  end
+    devise_parameter_sanitizer.for(:sign_up) do |u|
+      u.permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    end
+ end
 
 end
 
