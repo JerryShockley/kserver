ece5b7ed799c152468c0e378412fdfd49011119c
Jerry Shockley <jerry@shockleynet.com>
2014-09-08 15:11:51 -0400

    Added strong params support for our custom devise params :first_name & :loast_name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,24 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  #TODO Limit before_action to the appropriate devise actions
+  before_action :configure_devise_params, if: :devise_controller?
   protect_from_forgery with: :exception
   include Pundit
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
+
+
+  protected
+
+    def configure_devise_params
+      devise_parameter_sanitizer.for(:sign_up) do |u|
+        u.permit(:first_name, :last_name, :email, :password, :password_confirmation)
+      end
+    end
+
+
+                                             
 
   private
 
