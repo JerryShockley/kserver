9e1d95e1db6616dc3fb047c7cb6546c5a966f509
Jerry Shockley <jerry@shockleynet.com>
2014-09-09 08:14:25 -0400

    Added support for additional fields first_name and last_name

--- spec/features/visitors/sign_up_spec.rb
+++ spec/features/visitors/sign_up_spec.rb
@@ -8,9 +8,10 @@ feature 'Sign Up', :devise do
   #   Given I am not signed in
   #   When I sign up with a valid email address and password
   #   Then I see a successful sign up message
-  scenario 'visitor can sign up with valid email address and password' do
-    sign_up_with('test@example.com', 'please123', 'please123')
-    expect(page).to have_content 'Welcome! You have signed up successfully.'
+  scenario 'visitor can sign up with valid first name, last name, email address and password' do
+    
+    sign_up_with('Joe', 'Allenator', 'test@example.com', 'please123', 'please123')
+    expect(page).to have_content 'Welcome to ColorSisters Joe!'
   end
 
   # Scenario: Visitor cannot sign up with invalid email address
@@ -18,7 +19,7 @@ feature 'Sign Up', :devise do
   #   When I sign up with an invalid email address
   #   Then I see an invalid email message
   scenario 'visitor cannot sign up with invalid email address' do
-    sign_up_with('bogus', 'please123', 'please123')
+    sign_up_with('Joe', 'Allenator', 'bogus', 'please123', 'please123')
     expect(page).to have_content 'Email is invalid'
   end
 
@@ -27,7 +28,7 @@ feature 'Sign Up', :devise do
   #   When I sign up without a password
   #   Then I see a missing password message
   scenario 'visitor cannot sign up without password' do
-    sign_up_with('test@example.com', '', '')
+    sign_up_with('Joe', 'Allenator', 'test@example.com', '', '')
     expect(page).to have_content "Password can't be blank"
   end
 
@@ -36,7 +37,7 @@ feature 'Sign Up', :devise do
   #   When I sign up with a short password
   #   Then I see a 'too short password' message
   scenario 'visitor cannot sign up with a short password' do
-    sign_up_with('test@example.com', 'please', 'please')
+    sign_up_with('Joe', 'Allenator', 'test@example.com', 'plea', 'plea')
     expect(page).to have_content "Password is too short"
   end
 
@@ -45,7 +46,7 @@ feature 'Sign Up', :devise do
   #   When I sign up without a password confirmation
   #   Then I see a missing password confirmation message
   scenario 'visitor cannot sign up without password confirmation' do
-    sign_up_with('test@example.com', 'please123', '')
+    sign_up_with('Joe', 'Allenator', 'test@example.com', 'please123', '')
     expect(page).to have_content "Password confirmation doesn't match"
   end
 
@@ -54,7 +55,7 @@ feature 'Sign Up', :devise do
   #   When I sign up with a mismatched password confirmation
   #   Then I should see a mismatched password message
   scenario 'visitor cannot sign up with mismatched password and confirmation' do
-    sign_up_with('test@example.com', 'please123', 'mismatch')
+    sign_up_with('Joe', 'Allenator', 'test@example.com', 'please123', 'mismatch')
     expect(page).to have_content "Password confirmation doesn't match"
   end
 
