8f0a442a03158b2c0e46b487545c40e7d3deb756
Jerry Shockley <jerry@shockleynet.com>
2014-09-09 08:15:06 -0400

    Updated to reflect the new role names as part of repurposing this app. Added todo.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -51,11 +51,12 @@ describe User do
         fname = "Joe"
         lname = "Smith"
         name = "Joe Smith"
-        usr = build_stubbed(:parent, {first_name: fname, last_name: lname})
+        usr = build_stubbed(:writer, {first_name: fname, last_name: lname})
         expect(usr.name).to match(name)
         
       end
     end
+    
   
 
     
@@ -69,46 +70,46 @@ describe User do
         expect(sadmin.admin?).to be_truthy
       end
   
-      it "returns false for teacher" do
-        usr = build_stubbed(:teacher)
+      it "returns false for editor" do
+        usr = build_stubbed(:editor)
         expect(usr.admin?).to be_falsey
       end
   
-      it "returns false for Parent" do
-        usr = build_stubbed(:parent)
+      it "returns false for writer" do
+        usr = build_stubbed(:writer)
         expect(usr.admin?).to be_falsey
       end
   
-      it "returns false for Student" do
-        usr = build_stubbed(:student)
+      it "returns false for cust" do
+        usr = build_stubbed(:cust)
         expect(usr.admin?).to be_falsey
       end
     end
 
   
-    describe "faculty?" do
+    describe "staff?" do
       it "returns true for administrator" do
         usr = build_stubbed(:administrator)
-        expect(usr.faculty?).to be_truthy
+        expect(usr.staff?).to be_truthy
       end
   
       it "returns true for sysadmin" do
-        expect(sadmin.faculty?).to be_truthy
+        expect(sadmin.staff?).to be_truthy
       end
   
-      it "returns true for teacher" do
-        usr = build_stubbed(:teacher)
-        expect(usr.faculty?).to be_truthy
+      it "returns true for editor" do
+        usr = build_stubbed(:editor)
+        expect(usr.staff?).to be_truthy
       end
   
-      it "returns false for Parent" do
-        usr = build_stubbed(:parent)
-        expect(usr.faculty?).to be_falsey
+      it "returns false for writer" do
+        usr = build_stubbed(:writer)
+        expect(usr.staff?).to be_truthy
       end
   
-      it "returns false for Student" do
-        usr = build_stubbed(:student)
-        expect(usr.faculty?).to be_falsey
+      it "returns false for cust" do
+        usr = build_stubbed(:cust)
+        expect(usr.staff?).to be_falsey
       end
     end
   
