d6ac41dc57c4c4f254483e706bf451b65357e4d1
Jerry Shockley <jerry@shockleynet.com>
2014-09-24 18:58:00 -0400

    Added tests for edit and delete links

--- spec/features/users/user_index_spec.rb
+++ spec/features/users/user_index_spec.rb
@@ -1,5 +1,4 @@
-include Warden::Test::Helpers
-Warden.test_mode!
+require 'rails_helper'
 
 # Feature: User index page
 #   As a user
@@ -9,6 +8,7 @@ feature 'User index page', :devise do
 
   after(:each) do
     Warden.test_reset!
+    # @request.env["devise.mapping"] = Devise.mappings[:user]
   end
 
   # Scenario: User listed on index page
@@ -16,11 +16,38 @@ feature 'User index page', :devise do
   #   When I visit the user index page
   #   Then I see my own email address
   scenario 'user sees own email address' do
-    user = FactoryGirl.create(:user, :administrator)
+    user = create :administrator
     login_as(user, scope: :user)
     visit users_path
     expect(page).to have_content user.email
+    expect(page).to have_selector "a", text: "Edit"
+    expect(page).to have_selector "a", text: "Delete"    
   end
-  # TODO Add test for linked cols show, edit & del
+  
+  
+  scenario 'admin edits another user account' do
+    user = create :administrator
+    other_user = create :cust
+    login_as(user, scope: :user)
+    visit users_path
+    click_link "edit_#{other_user.id}"
+    expect(page).to have_content "Edit User"
+    expect(page).to have_field "user_email", with: other_user.email
+    
+  end
+  
+  
+  scenario 'admin deletes another user account' do
+    user = create :administrator
+    other_user = create :cust
+    login_as(user, scope: :user)
+    visit users_path
+    click_link "delete_#{other_user.id}"
+    #TODO Add the appropriate expectation here after fixing registration_controller destroy render problem
+    # expect(page).to have_content other_user.email
+
+  end
+  
+  
 
 end
