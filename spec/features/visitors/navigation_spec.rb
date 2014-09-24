37dc161384d63f64245dc86c2fe72fea51b05ceb
Jerry Shockley <jerry@shockleynet.com>
2014-09-24 19:00:24 -0400

    Conform to rails standard testing.

--- spec/features/visitors/navigation_spec.rb
+++ spec/features/visitors/navigation_spec.rb
@@ -1,3 +1,5 @@
+require 'rails_helper'
+
 # Feature: Navigation links
 #   As a visitor
 #   I want to see navigation links
@@ -9,7 +11,7 @@ feature 'Navigation links', :devise do
   #   When I visit the home page
   #   Then I see "home," "sign in," and "sign up"
   scenario 'view navigation links' do
-    visit root_path
+    visit root_url
     expect(page).to have_content 'ColorSisters'
     expect(page).to have_content 'Sign in'
     expect(page).to have_content 'Sign up'
