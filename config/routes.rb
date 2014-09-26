b2e0bd51113cc27f158abec5708d0de41ac3793f
Jerry Shockley <jerry@shockleynet.com>
2014-09-25 20:15:36 -0400

    Removed references to UsersController after conslidation with Devise::RegistrationsController. Cleaned up some commented out work.

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   # actions enble a user to edit another user's profile
 
   devise_scope :user  do
+    get "/account/users" => "devise/registrations#index", as: :users
     get "/account/users/:id" => "devise/registrations#show", as: :userx_registration
     get "/account/users/edit/:id" => "devise/registrations#edit", as: :edit_userx_registration
     put "/account/users/:id" => "devise/registrations#update"
@@ -17,32 +18,7 @@ Rails.application.routes.draw do
   end
 
 
-  # resources :registrations, only: :index
-  
   devise_for :users, path: 'account'
-
-  # devise_for :users, :skip => [:registrations]
-  #   as :user do
-  #     get 'cancel_user_registration' => 'devise/sessions#new', :as => :new_user_session
-  #     post 'signin' => 'devise/sessions#create', :as => :user_session
-  #     delete 'signout' => 'devise/sessions#destroy', :as => :destroy_user_session
-  #   end
-
-
-
-
-  resources :users
-#   devise_scope :user do
-#
-#     Note we use the route name 'user' for our route extensions (#index & #show), rather than
-#     user_registration as the other devise actions use. We do this due to a name
-#     conflict , for #create and #show. Devise incorrectly uses user_registration
-#     for #create when the standard is to use user_registrations for this action.
-#     The name user_registration rightly belongs to #show action.
-#     get "/users" => 'registrations#index', as: "users"
-#     get "/users/:id" => 'registrations#show', as: "user"
-#     get "/users"
-#   end
        
   # resources :users # do
 #     collection do
