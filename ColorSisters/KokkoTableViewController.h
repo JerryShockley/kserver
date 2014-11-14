82c8afc27e9f33d9f05ed79f2952cdb952590b71
Rob Chohan <Rob@RobLabs.com>
2014-11-13 20:36:24 -0800

    UI is functionally complete compared to Proto.io user interface

---
+++ ColorSisters/KokkoTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  KokkoTableViewController.h
+//  ColorSisters
+//
+//  Created by Rob Chohan on 11/13/14.
+//  Copyright (c) 2014 Kokko, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KokkoTableViewController : UITableViewController
+
+@property (nonatomic, retain) NSArray *titleMatchesArray;
+@property (nonatomic, retain) NSArray *subtitleMatchesArray;
+@end
