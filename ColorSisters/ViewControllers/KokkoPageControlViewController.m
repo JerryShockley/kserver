79a4a148139f3372d0367efd06f0871d504876b6
Rob Chohan <Rob@RobLabs.com>
2014-11-24 13:14:17 -0800

    Launch - Initial Page Control Viewer for

--- ColorSisters/ViewControllers/KokkoPageControlViewController.m
+++ ColorSisters/ViewControllers/KokkoPageControlViewController.m
@@ -8,6 +8,10 @@
 #import "KokkoPageControlViewController.h"
 
 @interface KokkoPageControlViewController ()
+@property (weak, nonatomic) IBOutlet UIPageControl *pageControl;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+
+@property (strong, nonatomic) UIImageView *currentView;
 
 @end
 
@@ -15,8 +19,38 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+
+    self.pageControl.pageIndicatorTintColor = [UIColor blueColor];
+    self.pageControl.currentPageIndicatorTintColor = [UIColor redColor];
+
+
+    
+    self.imageView.animationImages = @[
+                                  [UIImage imageNamed:@"Page1"],
+                                  [UIImage imageNamed:@"Page2"],
+                                  [UIImage imageNamed:@"Page3"],
+                                  ];
+    
+
+    self.imageView.image = self.imageView.animationImages[0];
+
+    
+    // We want the image to be scaled to the correct aspect ratio within imageView's bounds.
+    self.imageView.contentMode = UIViewContentModeScaleAspectFit;
+    
+    // If the image does not have the same aspect ratio as imageView's bounds, then imageView's backgroundColor will be applied to the "empty" space.
+    self.imageView.backgroundColor = [UIColor whiteColor];
 }
+
+
+- (IBAction)pageControlChanged:(id)sender {
+    [self.currentView setHidden:YES];
+    NSInteger selectedPage = self.pageControl.currentPage;
+    
+    self.imageView.image = self.imageView.animationImages[selectedPage];
+//    [self.currentView setHidden:NO];
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
