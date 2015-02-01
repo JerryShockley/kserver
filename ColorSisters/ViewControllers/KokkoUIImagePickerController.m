72b1f6478c128944f80ba417bf7391c71e4ba830
Paul Lettieri <paul@octalcreative.com>
2015-02-01 09:15:45 -0800

    Draws a box around the face and the chart.  Not yet animated or fancy in any way.  But it does demonstrate, at least with the two canned images, that the boxes are in the right place (after proper scaling to account for the scaling of the image as drawn on the screen). Also, does not actually draw until the end of the analysis, since the analysis is going on in the main thread, no UI can happen until it returns.  This, by the way, is why the spinner never appears.  The calculations are never giving any UI a chance to run.  We need to get the calculations into another thread for any useful UI to go on here.

--- ColorSisters/ViewControllers/KokkoUIImagePickerController.m
+++ ColorSisters/ViewControllers/KokkoUIImagePickerController.m
@@ -17,6 +17,11 @@
 @property (nonatomic, strong) UIActivityIndicatorView *spinner;
 @property (nonatomic, strong) UIButton *findFoundationButton;
 
+@property (nonatomic, strong) UIView *faceBox;
+@property (nonatomic, strong) UIView *chartBox;
+@property (nonatomic, readonly) CGFloat imageScale;
+
+
 // Matches dictionary to be passed to the table view
 @property (nonatomic) NSDictionary *shadeMatches;
 
@@ -56,6 +61,32 @@
         [_spinner setTranslatesAutoresizingMaskIntoConstraints:NO];
     }
     return _spinner;
+}
+
+
+- (UIView *)faceBox
+{
+    if (!_faceBox) {
+        self.faceBox = [[UIView alloc] init];
+        _faceBox.layer.borderColor = UIColor.whiteColor.CGColor;
+        _faceBox.layer.borderWidth = 2;
+    }
+    return _faceBox;
+}
+
+- (UIView *)chartBox
+{
+    if (!_chartBox) {
+        self.chartBox = [[UIView alloc] init];
+        _chartBox.layer.borderColor = UIColor.whiteColor.CGColor;
+        _chartBox.layer.borderWidth = 2;
+    }
+    return _chartBox;
+}
+
+- (CGFloat)imageScale
+{
+    return self.imageView.image.size.width / self.view.frame.size.width;
 }
 
 
@@ -139,17 +170,27 @@
     [[KokkoInterface sharedKokkoInterface] analyzeImage:self.image delegate:self];
 }
 
+- (CGRect)scaleRect:(CGRect)r byFactor:(CGFloat)f
+{
+    return CGRectMake(r.origin.x/f, r.origin.y/f, r.size.width/f, r.size.height/f);
+}
 
 #pragma mark - Kokko Image Processing Delegate
 
 - (void)kokkoInterface:(KokkoInterface *)kokkoInterface foundFaceRect:(CGRect)faceRect
 {
-    NSLog(@"Found face at %.1f,%.1f",faceRect.origin.x, faceRect.origin.y);
+    NSLog(@"Found face at (%.1f,%.1f) with size (%.1f,%.1f)",faceRect.origin.x, faceRect.origin.y, faceRect.size.width, faceRect.size.height);
+
+    self.faceBox.frame = [self scaleRect:faceRect byFactor:self.imageScale];
+    [self.imageView addSubview:self.faceBox];
 }
 
 - (void)kokkoInterface:(KokkoInterface *)kokkoInterface foundChartRect:(CGRect)chartRect
 {
-    NSLog(@"Found chart at %.1f,%.1f",chartRect.origin.x, chartRect.origin.y);
+    NSLog(@"Found chart at (%.1f,%.1f) with size (%.1f,%.1f)",chartRect.origin.x, chartRect.origin.y, chartRect.size.width, chartRect.size.height);
+
+    self.chartBox.frame = [self scaleRect:chartRect byFactor:self.imageScale];
+    [self.imageView addSubview:self.chartBox];
 }
 
 - (void)kokkoInterface:(KokkoInterface *)kokkoInterface analysisProgress:(CGFloat)progress
